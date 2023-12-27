0049084fb6955f814ff2f2935c82625a57d64713
jindou <xiejindou@rightchaintech.com>
2023-12-27 17:35:42 +0800

    update

--- rsync/start.sh
+++ rsync/start.sh
@@ -5,17 +5,21 @@ name=rsync
 data=$(cd `dirname $0`;pwd)/data
 # Where you need to sync
 rsync_place=/anywhere
+mount_place=/mnt/xxxx
 image=xiejindou/rsyncssh:alpine
+port=2222
+
 
 if docker ps -a --format '{{.Names}}' | grep -q ${name}; then
      docker stop ${name} && docker rm ${name}
 fi
 
 docker run --name ${name} \
--p 2222:22 \
+-p ${port}:22 \
 -v ${data}/authorized_keys:/home/user/.ssh/authorized_keys \
--v ${rsync_place}:/mnt/xxxx \
+-v ${rsync_place}:${mount_place} \
 -d ${image}
 
 docker exec -it ${name} chmod 600 /home/user/.ssh/authorized_keys
 docker exec -it ${name} chown user:user /home/user/.ssh/authorized_keys
+docker exec -it ${name} chown user:user ${mount_place}
